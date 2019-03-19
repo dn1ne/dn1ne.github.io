f5f5186777f0030805f364d4bbf1bd415e81b1f0
datnh <datnh@s-wifi.vn>
2019-03-19 08:19:04 +0700

    Updates

---
+++ precache-manifest.ee60c22a6ffbe4d08b880a0026f48593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1eb2062a7e69cb9cffe",
+    "url": "/static/js/main.a721aa00.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "d1eb2062a7e69cb9cffe",
+    "url": "/static/css/main.34466942.chunk.css"
+  },
+  {
+    "revision": "5ff64ff14d394312fff0e5b4056850cb",
+    "url": "/index.html"
+  }
+];
